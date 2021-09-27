248a641dceed0d7b8baef369f66b591f3215829d
Nata chikhinashvili <natanatuka33@gmail.com>
2021-09-27 22:32:03 +0400

    fix: 1algorithm about promises

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -80,8 +80,8 @@ function processAllPromises(array) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -104,6 +104,7 @@ function getFastestPromise(/* array */) {
 function chainPromises(/* array, action */) {
   throw new Error('Not implemented');
 }
+
 
 module.exports = {
   willYouMarryMe,
